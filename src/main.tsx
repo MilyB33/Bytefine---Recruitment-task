98c33d5381ec1eebde8d689e5af75b84a2a87b43
Miłosz Bolewski <milosz.bolewski.33@gmail.com>
2025-01-30 19:18:41 +0100

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <p className="bg-red-100">dsds</p>
+  </StrictMode>
+);
